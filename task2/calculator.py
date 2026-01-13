a3f0bccd832476692c071985734f808cba42fb42
tdabhade44-bit <tdabhade44@gmail.com>
2026-01-13 13:37:47 +0530

    Add files via upload

---
+++ task2/calculator.py
@@ -0,0 +1,32 @@
+print("SIMPLE CALCULATOR")
+
+# Taking inputs
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("\nChoose Operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+choice = input("Enter choice(1/2/3/4): ")
+
+# Calculation
+if choice == "1":
+    print("Result:", num1 + num2)
+
+elif choice == "2":
+    print("Result:", num1 - num2)
+
+elif choice == "3":
+    print("Result:", num1 * num2)
+
+elif choice == "4":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero not allowed")
+
+else:
+    print("Invalid choice")
